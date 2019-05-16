136a1f4efb84102e20f1a3170cd035178e90a43c
Linda <48590196+Punzel@users.noreply.github.com>
2019-05-16 11:27:35 +0300

    Add files via upload

--- planty.py
+++ planty.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template, url_for, redirect, flash, request, session
 import config
-from forms import add_plant_form
+from forms import add_plant_form, AddPictureForm
 from database import db_session
 from models import Plants
 from sqlalchemy import asc, func, desc
 from flask_table import Table, Col
+import os
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+
+UPLOAD_FOLDER = "./static"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.debug = True
 app.config.from_object(config)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+UPLOAD_FOLDER = "./static"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 #index
 @app.route('/')
 def index():
     return render_template('index.jinja')
+ 
  
 # plants page. Shows all plants in database 
 @app.route("/plants")
@@ -45,28 +56,42 @@ def newest_plant():
     newest = db_session.query(Plants).filter(Plants.id == db_session.query(func.max(Plants.id)))
     return render_template ('newest_plant.jinja', newest=newest)
 
+# checks for allowed files
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 #picture upload
 @app.route("/upload_picture", methods=["GET", "POST"])
 def upload_picture():
-    form = AddPictureForm()
-    if form.validate_on_submit():
-        filename = secure_filename(form.image.data.filename)    
-        new_picture = Picture(picturepath='./static/'+filename, plant_picture=form.picturepath.data)
-        db_session.add(new_picture)
-        db_session.commit()
-        form.image.data.save('./static/' + filename)
-        return redirect(url_for('plants'))
-    else:
-        print "could not be added. Error!"
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('plants', filename=filename))
+    return render_template ('upload_picture.jinja')    
 
+#admin function for editing information
 @app.route("/plants_admin", methods=["GET", "POST"])      
 def plants_admin():
     data = Plants.query.all()
     return render_template('plants_admin.jinja', data=data)
 
+'''
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-''' 
 def user_add():
     form = NewUserForm()
     if form.validate_on_submit():
